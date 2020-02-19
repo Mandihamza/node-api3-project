00078bd8ab34ce6733da0a9940f64e045384a2bb
mandihamza <mandihamza@gmail.com>
2020-02-19 16:43:50 -0700

    Adds route to GET users array at /api/users

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,56 @@
 const express = require('express');
-
 const router = express.Router();
+const db = require('./userDb')
 
 router.post('/', (req, res) => {
-  // do your magic!
+  
 });
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+  
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get('/', (req, res, next) => {
+  db.get()
+  .then((users) => {
+    res
+    .status(200)
+    .json(users)
+  }).catch((err) => {
+    //console.log(err)
+    res.status(500).json({ message: "Error retrieving the hubs"})
+    next()
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  
 }
 
 module.exports = router;
