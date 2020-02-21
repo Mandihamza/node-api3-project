ff70a6eaafd7083e60c39298fe47dc35caac6cba
mandihamza <mandihamza@gmail.com>
2020-02-20 17:20:57 -0700

    Adds PUT route for /api/users:id

--- users/userRouter.js
+++ users/userRouter.js
@@ -67,8 +67,22 @@ router.delete('/:id', (req, res) => {
   });
 });
 
-router.put('/:id', (req, res) => {
-  
+router.put('/:id', (req, res, next) => {
+  db.update(req.params.id, req.body)
+  .then(users => {
+    if (users) {
+      res.status(200).json(users);
+    } else {
+      res.status(404).json({ message: 'The user could not be found' });
+    }
+  })
+  .catch(error => {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error updating the user',
+    });
+  });
 });
 
 //custom middleware
